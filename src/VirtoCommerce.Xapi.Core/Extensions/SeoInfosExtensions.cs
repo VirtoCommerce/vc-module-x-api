dabd707ae67d01f5716c19a5801b68561925073c
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-07-05 13:05:14 +0200

    feat: Seo Search  by permalink

--- src/VirtoCommerce.Xapi.Core/Extensions/SeoInfosExtensions.cs
+++ src/VirtoCommerce.Xapi.Core/Extensions/SeoInfosExtensions.cs
@@ -3,84 +3,86 @@ using System.Linq;
 using VirtoCommerce.CoreModule.Core.Seo;
 using VirtoCommerce.Platform.Core.Common;
 
-namespace VirtoCommerce.Xapi.Core.Extensions
+namespace VirtoCommerce.Xapi.Core.Extensions;
+
+public static class SeoInfosExtensions
 {
-    public static class SeoInfosExtensions
+    public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string storeId, string cultureName)
     {
-        public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string storeId, string cultureName)
-        {
-            if (storeId == null || cultureName == null)
-            {
-                return null;
-            }
+        return GetBestMatchingSeoInfoInternal(seoInfos, storeId, cultureName, cultureName, slug: null, permalink: null);
+    }
 
-            return GetBestMatchingSeoInfoInternal(seoInfos, storeId, cultureName, cultureName, null);
+    public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string storeId, string cultureName, string slug)
+    {
+        return GetBestMatchingSeoInfoInternal(seoInfos, storeId, cultureName, cultureName, slug, permalink: null);
+    }
+
+    public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string storeId, string defaultStoreLang, string cultureName, string slug)
+    {
+        return GetBestMatchingSeoInfoInternal(seoInfos, storeId, defaultStoreLang, cultureName, slug, permalink: null);
+    }
+
+    public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string storeId, string defaultStoreLang, string cultureName, string slug, string permalink)
+    {
+        return GetBestMatchingSeoInfoInternal(seoInfos, storeId, defaultStoreLang, cultureName, slug, permalink);
+    }
+
+    public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string defaultStoreLang,
+        SeoSearchCriteria criteria)
+    {
+        return GetBestMatchingSeoInfoInternal(seoInfos, criteria.StoreId, defaultStoreLang, criteria.LanguageCode,
+            criteria.Slug, criteria.Permalink);
+    }
+
+    private static SeoInfo GetBestMatchingSeoInfoInternal(IList<SeoInfo> seoInfos, string storeId,
+        string defaultStoreLang, string cultureName, string slug, string permalink)
+    {
+        if (storeId.IsNullOrEmpty() || cultureName.IsNullOrEmpty() || slug.IsNullOrEmpty())
+        {
+            return null;
         }
+        return seoInfos.GetBestMatchingSeoInfos(storeId, defaultStoreLang, cultureName, slug, permalink);
+    }
 
-        public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string storeId, string cultureName, string slug)
+    public static SeoInfo GetFallbackSeoInfo(string id, string name, string cultureName)
+    {
+        var result = AbstractTypeFactory<SeoInfo>.TryCreateInstance();
+        result.SemanticUrl = id;
+        result.LanguageCode = cultureName;
+        result.Name = name;
+        return result;
+    }
+
+    private static SeoInfo GetBestMatchingSeoInfos(this IEnumerable<SeoInfo> seoRecords, string storeId, string defaultStoreLang, string language, string slug, string permalink)
+    {
+        var result = seoRecords?.Select(s => new
         {
-            if (storeId == null || cultureName == null || slug == null)
-            {
-                return null;
-            }
+            SeoRecord = s,
+            Score = CalculateScore(s, slug, permalink, storeId, defaultStoreLang, language)
+        })
+        .OrderByDescending(x => x.Score)
+        .Select(x => x.SeoRecord)
+        .FirstOrDefault();
 
-            return GetBestMatchingSeoInfoInternal(seoInfos, storeId, cultureName, cultureName, slug);
-        }
+        return result;
+    }
 
-        public static SeoInfo GetBestMatchingSeoInfo(this IList<SeoInfo> seoInfos, string storeId, string defaultStoreLang, string cultureName, string slug)
+    private static int CalculateScore(SeoInfo seoInfo, string slug, string permalink, string storeId, string defaultStoreLang, string language)
+    {
+        var score = new[]
         {
-            if (storeId == null || cultureName == null || slug == null)
-            {
-                return null;
-            }
-
-            return GetBestMatchingSeoInfoInternal(seoInfos, storeId, defaultStoreLang, cultureName, slug);
-        }
-
-        private static SeoInfo GetBestMatchingSeoInfoInternal(IList<SeoInfo> seoInfos, string storeId, string defaultStoreLang, string cultureName, string slug)
-        {
-            return seoInfos.GetBestMatchingSeoInfos(storeId, defaultStoreLang, cultureName, slug);
-        }
-
-        public static SeoInfo GetFallbackSeoInfo(string id, string name, string cultureName)
-        {
-            var result = AbstractTypeFactory<SeoInfo>.TryCreateInstance();
-            result.SemanticUrl = id;
-            result.LanguageCode = cultureName;
-            result.Name = name;
-            return result;
-        }
-
-        private static SeoInfo GetBestMatchingSeoInfos(this IEnumerable<SeoInfo> seoRecords, string storeId, string defaultStoreLang, string language, string slug)
-        {
-            var result = seoRecords?.Select(s => new
-            {
-                SeoRecord = s,
-                Score = CalculateScore(s, slug, storeId, defaultStoreLang, language)
-            })
-            .OrderByDescending(x => x.Score)
-            .Select(x => x.SeoRecord)
-            .FirstOrDefault();
-
-            return result;
-        }
-
-        private static int CalculateScore(SeoInfo seoInfo, string slug, string storeId, string defaultStoreLang, string language)
-        {
-            var score = new[]
-            {
                 seoInfo.IsActive,
-                !string.IsNullOrEmpty(slug) && slug.EqualsInvariant(seoInfo.SemanticUrl),
+                !string.IsNullOrEmpty(permalink) && permalink.TrimStart('/').EqualsInvariant(seoInfo.SemanticUrl.TrimStart('/')),
+                !string.IsNullOrEmpty(slug) && slug.TrimStart('/').EqualsInvariant(seoInfo.SemanticUrl.TrimStart('/')),
                 storeId.EqualsInvariant(seoInfo.StoreId),
                 language.Equals(seoInfo.LanguageCode),
                 defaultStoreLang.EqualsInvariant(seoInfo.LanguageCode),
                 seoInfo.LanguageCode.IsNullOrEmpty()
             }
-            .Reverse()
-            .Select((valid, index) => valid ? 1 << index : 0)
-            .Sum();
+        .Reverse()
+        .Select((valid, index) => valid ? 1 << index : 0)
+        .Sum();
 
-            return score;
-        }
+        return score;
     }
 }
