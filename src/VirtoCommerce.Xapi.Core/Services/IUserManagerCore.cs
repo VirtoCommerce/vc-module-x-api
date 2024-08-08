0e42cff7a8589f87ee08069863c2952cefd19260
artem-dudarev <ad@virtoway.com>
2024-08-08 11:49:51 +0200

    Don't throw PasswordExpired when using external sign in

--- src/VirtoCommerce.Xapi.Core/Services/IUserManagerCore.cs
+++ src/VirtoCommerce.Xapi.Core/Services/IUserManagerCore.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using GraphQL;
 using VirtoCommerce.Platform.Core.Security;
 
 namespace VirtoCommerce.Xapi.Core.Services
@@ -7,6 +9,9 @@ namespace VirtoCommerce.Xapi.Core.Services
     {
         Task<bool> IsLockedOutAsync(ApplicationUser user);
 
+        [Obsolete("Use CheckCurrentUserState()", DiagnosticId = "VC0009", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         Task CheckUserState(string userId, bool allowAnonymous);
+
+        Task CheckCurrentUserState(IResolveFieldContext context, bool allowAnonymous);
     }
 }
