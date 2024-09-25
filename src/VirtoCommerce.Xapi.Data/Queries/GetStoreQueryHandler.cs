305e892641ac3dc0aec9c611177900cc44e2baee
Oleg Zhuk <zhukoo@gmail.com>
2024-09-25 11:19:49 +0200

    VCST-1880: Adds the support for dictionary public property.
    fix: Adds the support for dictionary public property. Value property will return json string that can be decoded using JSON.parse.
    
    ```json
    "settings": [
                  {
                    "name": "Test.DisableTrackingDomains",
                    "value": "[\"virtoworks.com\",\"teste\",\"virtoway.com\"]"
                  }
                ]
    ```json

--- src/VirtoCommerce.Xapi.Data/Queries/GetStoreQueryHandler.cs
+++ src/VirtoCommerce.Xapi.Data/Queries/GetStoreQueryHandler.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using VirtoCommerce.CoreModule.Core.Common;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Settings;
@@ -18,160 +19,166 @@ using VirtoCommerce.Xapi.Core.Subscriptions;
 using StoreSettingGeneral = VirtoCommerce.StoreModule.Core.ModuleConstants.Settings.General;
 using StoreSettingSeo = VirtoCommerce.StoreModule.Core.ModuleConstants.Settings.SEO;
 
-namespace VirtoCommerce.Xapi.Data.Queries
+namespace VirtoCommerce.Xapi.Data.Queries;
+
+public class GetStoreQueryHandler : IQueryHandler<GetStoreQuery, StoreResponse>
 {
-    public class GetStoreQueryHandler : IQueryHandler<GetStoreQuery, StoreResponse>
+    private readonly IStoreService _storeService;
+    private readonly IStoreSearchService _storeSearchService;
+    private readonly IStoreCurrencyResolver _storeCurrencyResolver;
+    private readonly ISettingsManager _settingsManager;
+    private readonly IdentityOptions _identityOptions;
+    private readonly GraphQLWebSocketOptions _webSocketOptions;
+    private readonly StoresOptions _storeOptions;
+    private readonly IStoreAuthenticationService _storeAuthenticationService;
+
+    public GetStoreQueryHandler(
+        IStoreService storeService,
+        IStoreSearchService storeSearchService,
+        IStoreCurrencyResolver storeCurrencyResolver,
+        ISettingsManager settingsManager,
+        IOptions<IdentityOptions> identityOptions,
+        IOptions<GraphQLWebSocketOptions> webSocketOptions,
+        IOptions<StoresOptions> storeOptions,
+        IStoreAuthenticationService storeAuthenticationService)
+
     {
-        private readonly IStoreService _storeService;
-        private readonly IStoreSearchService _storeSearchService;
-        private readonly IStoreCurrencyResolver _storeCurrencyResolver;
-        private readonly ISettingsManager _settingsManager;
-        private readonly IdentityOptions _identityOptions;
-        private readonly GraphQLWebSocketOptions _webSocketOptions;
-        private readonly StoresOptions _storeOptions;
-        private readonly IStoreAuthenticationService _storeAuthenticationService;
+        _storeService = storeService;
+        _storeSearchService = storeSearchService;
+        _storeCurrencyResolver = storeCurrencyResolver;
+        _settingsManager = settingsManager;
+        _storeAuthenticationService = storeAuthenticationService;
+        _identityOptions = identityOptions.Value;
+        _webSocketOptions = webSocketOptions.Value;
+        _storeOptions = storeOptions.Value;
+    }
 
-        public GetStoreQueryHandler(
-            IStoreService storeService,
-            IStoreSearchService storeSearchService,
-            IStoreCurrencyResolver storeCurrencyResolver,
-            ISettingsManager settingsManager,
-            IOptions<IdentityOptions> identityOptions,
-            IOptions<GraphQLWebSocketOptions> webSocketOptions,
-            IOptions<StoresOptions> storeOptions,
-            IStoreAuthenticationService storeAuthenticationService)
+    public async Task<StoreResponse> Handle(GetStoreQuery request, CancellationToken cancellationToken)
+    {
+        Store store = null;
 
+        if (!string.IsNullOrEmpty(request.StoreId))
         {
-            _storeService = storeService;
-            _storeSearchService = storeSearchService;
-            _storeCurrencyResolver = storeCurrencyResolver;
-            _settingsManager = settingsManager;
-            _storeAuthenticationService = storeAuthenticationService;
-            _identityOptions = identityOptions.Value;
-            _webSocketOptions = webSocketOptions.Value;
-            _storeOptions = storeOptions.Value;
+            store = await _storeService.GetByIdAsync(request.StoreId, clone: false);
+        }
+        else if (!string.IsNullOrEmpty(request.Domain))
+        {
+            store = await ResolveStoreByDomain(request.Domain);
+
+            if (store == null && !string.IsNullOrEmpty(_storeOptions.DefaultStore))
+            {
+                store = await _storeService.GetByIdAsync(_storeOptions.DefaultStore, clone: false);
+            }
         }
 
-        public async Task<StoreResponse> Handle(GetStoreQuery request, CancellationToken cancellationToken)
+        if (store == null)
         {
-            Store store = null;
+            return null;
+        }
 
-            if (!string.IsNullOrEmpty(request.StoreId))
+        var cultureName = request.CultureName ?? store.DefaultLanguage;
+
+        var allCurrencies = await _storeCurrencyResolver.GetAllStoreCurrenciesAsync(store.Id, cultureName);
+        var availableCurrencies = allCurrencies.Where(x => store.Currencies.Contains(x.Code)).ToList();
+        var defaultCurrency = await _storeCurrencyResolver.GetStoreCurrencyAsync(store.DefaultCurrency, store.Id, cultureName);
+
+        var defaultLanguage = store.DefaultLanguage != null ? new Language(store.DefaultLanguage) : Language.InvariantLanguage;
+        var availableLanguages = !store.Languages.IsNullOrEmpty() ? store.Languages.Select(x => new Language(x)).ToList() : new List<Language>();
+
+        var response = new StoreResponse
+        {
+            StoreId = store.Id,
+            StoreName = store.Name,
+            CatalogId = store.Catalog,
+            StoreUrl = store.Url,
+            DefaultCurrency = defaultCurrency,
+            AvailableCurrencies = availableCurrencies,
+            DefaultLanguage = defaultLanguage,
+            AvailableLanguages = availableLanguages,
+            GraphQLSettings = new GraphQLSettings
             {
-                store = await _storeService.GetByIdAsync(request.StoreId, clone: false);
+                KeepAliveInterval = _webSocketOptions.KeepAliveInterval,
             }
-            else if (!string.IsNullOrEmpty(request.Domain))
-            {
-                store = await ResolveStoreByDomain(request.Domain);
+        };
 
-                if (store == null && !string.IsNullOrEmpty(_storeOptions.DefaultStore))
+        if (!store.Settings.IsNullOrEmpty())
+        {
+            response.Settings = new StoreSettings
+            {
+                IsSpa = store.Settings.GetValue<bool>(StoreSettingGeneral.IsSpa),
+                TaxCalculationEnabled = store.Settings.GetValue<bool>(StoreSettingGeneral.TaxCalculationEnabled),
+                AnonymousUsersAllowed = store.Settings.GetValue<bool>(StoreSettingGeneral.AllowAnonymousUsers),
+                EmailVerificationEnabled = store.Settings.GetValue<bool>(StoreSettingGeneral.EmailVerificationEnabled),
+                EmailVerificationRequired = store.Settings.GetValue<bool>(StoreSettingGeneral.EmailVerificationRequired),
+                SeoLinkType = store.Settings.GetValue<string>(StoreSettingSeo.SeoLinksType),
+                CreateAnonymousOrderEnabled = store.Settings.GetValue<bool>(ModuleConstants.Settings.General.CreateAnonymousOrder),
+                DefaultSelectedForCheckout = store.Settings.GetValue<bool>(ModuleConstants.Settings.General.IsSelectedForCheckout),
+
+                QuotesEnabled = store.Settings.GetValue<bool>(new SettingDescriptor { Name = "Quotes.EnableQuotes" }),
+                SubscriptionEnabled = store.Settings.GetValue<bool>(new SettingDescriptor { Name = "Subscription.EnableSubscriptions" }),
+
+                EnvironmentName = _settingsManager.GetValue<string>(ModuleConstants.Settings.General.EnvironmentName),
+                PasswordRequirements = _identityOptions.Password,
+
+                AuthenticationTypes = (await _storeAuthenticationService.GetStoreSchemesAsync(store.Id))
+                    .Where(x => x.IsActive)
+                    .Select(x => x.Name)
+                    .ToList(),
+
+                Modules = ToModulesSettings(store.Settings)
+            };
+        }
+
+        return response;
+    }
+
+    protected virtual async Task<Store> ResolveStoreByDomain(string domain)
+    {
+        if (_storeOptions.Domains.TryGetValue(domain, out var storeId))
+        {
+            return await _storeService.GetByIdAsync(storeId, clone: false);
+        }
+
+        var criteria = AbstractTypeFactory<StoreSearchCriteria>.TryCreateInstance();
+        criteria.Domain = domain;
+        criteria.Take = 1;
+
+        var result = await _storeSearchService.SearchAsync(criteria, clone: false);
+        return result.Results.FirstOrDefault();
+    }
+
+    protected virtual ModuleSettings[] ToModulesSettings(ICollection<ObjectSettingEntry> settings)
+    {
+        var result = new List<ModuleSettings>();
+
+        foreach (var settingByModule in settings.Where(s => s.IsPublic).GroupBy(s => s.ModuleId))
+        {
+            var moduleSettings = new ModuleSettings
+            {
+                ModuleId = settingByModule.Key,
+                Settings = settingByModule.Select(s => new ModuleSetting
                 {
-                    store = await _storeService.GetByIdAsync(_storeOptions.DefaultStore, clone: false);
-                }
-            }
-
-            if (store == null)
-            {
-                return null;
-            }
-
-            var cultureName = request.CultureName ?? store.DefaultLanguage;
-
-            var allCurrencies = await _storeCurrencyResolver.GetAllStoreCurrenciesAsync(store.Id, cultureName);
-            var availableCurrencies = allCurrencies.Where(x => store.Currencies.Contains(x.Code)).ToList();
-            var defaultCurrency = await _storeCurrencyResolver.GetStoreCurrencyAsync(store.DefaultCurrency, store.Id, cultureName);
-
-            var defaultLanguage = store.DefaultLanguage != null ? new Language(store.DefaultLanguage) : Language.InvariantLanguage;
-            var availableLanguages = !store.Languages.IsNullOrEmpty() ? store.Languages.Select(x => new Language(x)).ToList() : new List<Language>();
-
-            var response = new StoreResponse
-            {
-                StoreId = store.Id,
-                StoreName = store.Name,
-                CatalogId = store.Catalog,
-                StoreUrl = store.Url,
-                DefaultCurrency = defaultCurrency,
-                AvailableCurrencies = availableCurrencies,
-                DefaultLanguage = defaultLanguage,
-                AvailableLanguages = availableLanguages,
-                GraphQLSettings = new GraphQLSettings
-                {
-                    KeepAliveInterval = _webSocketOptions.KeepAliveInterval,
-                }
+                    Name = s.Name,
+                    Value = ToSettingValue(s)
+                }).ToArray(),
             };
 
-            if (!store.Settings.IsNullOrEmpty())
+            if (moduleSettings.Settings.Length > 0)
             {
-                response.Settings = new StoreSettings
-                {
-                    IsSpa = store.Settings.GetValue<bool>(StoreSettingGeneral.IsSpa),
-                    TaxCalculationEnabled = store.Settings.GetValue<bool>(StoreSettingGeneral.TaxCalculationEnabled),
-                    AnonymousUsersAllowed = store.Settings.GetValue<bool>(StoreSettingGeneral.AllowAnonymousUsers),
-                    EmailVerificationEnabled = store.Settings.GetValue<bool>(StoreSettingGeneral.EmailVerificationEnabled),
-                    EmailVerificationRequired = store.Settings.GetValue<bool>(StoreSettingGeneral.EmailVerificationRequired),
-                    SeoLinkType = store.Settings.GetValue<string>(StoreSettingSeo.SeoLinksType),
-                    CreateAnonymousOrderEnabled = store.Settings.GetValue<bool>(ModuleConstants.Settings.General.CreateAnonymousOrder),
-                    DefaultSelectedForCheckout = store.Settings.GetValue<bool>(ModuleConstants.Settings.General.IsSelectedForCheckout),
-
-                    QuotesEnabled = store.Settings.GetValue<bool>(new SettingDescriptor { Name = "Quotes.EnableQuotes" }),
-                    SubscriptionEnabled = store.Settings.GetValue<bool>(new SettingDescriptor { Name = "Subscription.EnableSubscriptions" }),
-
-                    EnvironmentName = _settingsManager.GetValue<string>(ModuleConstants.Settings.General.EnvironmentName),
-                    PasswordRequirements = _identityOptions.Password,
-
-                    AuthenticationTypes = (await _storeAuthenticationService.GetStoreSchemesAsync(store.Id))
-                        .Where(x => x.IsActive)
-                        .Select(x => x.Name)
-                        .ToList(),
-
-                    Modules = ToModulesSettings(store.Settings)
-                };
+                result.Add(moduleSettings);
             }
-
-            return response;
         }
 
-        protected virtual async Task<Store> ResolveStoreByDomain(string domain)
+        return [.. result];
+    }
+
+    protected virtual object ToSettingValue(ObjectSettingEntry s)
+    {
+        if (s.IsDictionary)
         {
-            if (_storeOptions.Domains.TryGetValue(domain, out var storeId))
-            {
-                return await _storeService.GetByIdAsync(storeId, clone: false);
-            }
-
-            var criteria = AbstractTypeFactory<StoreSearchCriteria>.TryCreateInstance();
-            criteria.Domain = domain;
-            criteria.Take = 1;
-
-            var result = await _storeSearchService.SearchAsync(criteria, clone: false);
-            return result.Results.FirstOrDefault();
+            return JsonConvert.SerializeObject(s.AllowedValues ?? []);
         }
-
-        protected virtual ModuleSettings[] ToModulesSettings(ICollection<ObjectSettingEntry> settings)
-        {
-            var result = new List<ModuleSettings>();
-
-            foreach (var settingByModule in settings.Where(s => s.IsPublic).GroupBy(s => s.ModuleId))
-            {
-                var moduleSettings = new ModuleSettings
-                {
-                    ModuleId = settingByModule.Key,
-                    Settings = settingByModule.Select(s => new ModuleSetting
-                    {
-                        Name = s.Name,
-                        Value = ToSettingValue(s)
-                    }).ToArray(),
-                };
-
-                if (moduleSettings.Settings.Length > 0)
-                {
-                    result.Add(moduleSettings);
-                }
-            }
-
-            return result.ToArray();
-        }
-
-        protected virtual object ToSettingValue(ObjectSettingEntry s)
+        else
         {
             var result = s.Value ?? s.DefaultValue;
 
@@ -184,7 +191,6 @@ namespace VirtoCommerce.Xapi.Data.Queries
                         break;
                 }
             }
-
             return result;
         }
     }
