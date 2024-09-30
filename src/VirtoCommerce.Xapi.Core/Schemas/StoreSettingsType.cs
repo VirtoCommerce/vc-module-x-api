b8608ca07e21420254e8bfe474e6395db38421ab
Oleg Zhuk <zhukoo@gmail.com>
2024-09-30 16:44:08 +0200

    VCST-1867: Mark IsSpa and QuotesEnabled as Deprecated
    fix: Mark IsSpa, SubscriptionEnabled and QuotesEnabled as deprecated properties. Use Quotes.EnableQuotes public property instead. Client application should use own business logic for SPA detection. Both properties will be removed after 11 stable release.

--- src/VirtoCommerce.Xapi.Core/Schemas/StoreSettingsType.cs
+++ src/VirtoCommerce.Xapi.Core/Schemas/StoreSettingsType.cs
@@ -7,11 +7,11 @@ public class StoreSettingsType : ExtendableGraphType<StoreSettings>
 {
     public StoreSettingsType()
     {
-        Field(x => x.QuotesEnabled).Description("Quotes enabled");
-        Field(x => x.SubscriptionEnabled).Description("Store ID");
+        Field(x => x.QuotesEnabled).Description("Quotes enabled").DeprecationReason("Use Quotes.EnableQuotes public property instead.");
+        Field(x => x.SubscriptionEnabled).Description("Subscription enabled").DeprecationReason("Use Subscription.EnableSubscriptions public property instead.");
         Field(x => x.TaxCalculationEnabled).Description("Tax calculation enabled");
         Field(x => x.AnonymousUsersAllowed).Description("Allow anonymous users to visit the store ");
-        Field(x => x.IsSpa).Description("SPA");
+        Field(x => x.IsSpa).Description("SPA").DeprecationReason("Client application should use own business logic for SPA detection.");
         Field(x => x.EmailVerificationEnabled).Description("Email address verification enabled");
         Field(x => x.EmailVerificationRequired).Description("Email address verification required");
         Field(x => x.CreateAnonymousOrderEnabled).Description("Allow anonymous users to create orders (XAPI)");
