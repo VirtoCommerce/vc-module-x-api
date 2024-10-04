0c7b7e7af0737a7e74d934e9c44bb54ced236d0e
Oleg Zhuk <zhukoo@gmail.com>
2024-10-04 15:03:43 +0200

    VCST-1867: Mark IsSpa, SubscriptionEnabled and QuotesEnabled as Deprecated (#13)
    feat: Mark IsSpa, SubscriptionEnabled and QuotesEnabled as deprecated properties. Use Quotes.EnableQuotes public property instead. The client application should use its own business logic for SPA detection. Both properties will be removed after 11 stable releases.

--- src/VirtoCommerce.Xapi.Core/Models/StoreSettings.cs
+++ src/VirtoCommerce.Xapi.Core/Models/StoreSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
@@ -5,14 +6,17 @@ namespace VirtoCommerce.Xapi.Core.Models
 {
     public class StoreSettings
     {
+        [Obsolete("Use Quotes.EnableQuotes public property instead", DiagnosticId = "VC0009", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         public bool QuotesEnabled { get; set; }
 
+        [Obsolete("Use Subscription.EnableSubscriptions public property instead", DiagnosticId = "VC0009", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         public bool SubscriptionEnabled { get; set; }
 
         public bool TaxCalculationEnabled { get; set; }
 
         public bool AnonymousUsersAllowed { get; set; }
 
+        [Obsolete("Client application should use own business logic for SPA detection", DiagnosticId = "VC0009", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         public bool IsSpa { get; set; }
 
         public bool EmailVerificationEnabled { get; set; }
--- src/VirtoCommerce.Xapi.Core/Schemas/StoreSettingsType.cs
+++ src/VirtoCommerce.Xapi.Core/Schemas/StoreSettingsType.cs
@@ -7,11 +7,13 @@ public class StoreSettingsType : ExtendableGraphType<StoreSettings>
 {
     public StoreSettingsType()
     {
-        Field(x => x.QuotesEnabled).Description("Quotes enabled");
-        Field(x => x.SubscriptionEnabled).Description("Store ID");
+#pragma warning disable VC0009
+        Field(x => x.QuotesEnabled).Description("Quotes enabled").DeprecationReason("Use Quotes.EnableQuotes public property instead.");
+        Field(x => x.SubscriptionEnabled).Description("Subscription enabled").DeprecationReason("Use Subscription.EnableSubscriptions public property instead.");
+        Field(x => x.IsSpa).Description("SPA").DeprecationReason("Client application should use own business logic for SPA detection.");
+#pragma warning restore VC0009
         Field(x => x.TaxCalculationEnabled).Description("Tax calculation enabled");
         Field(x => x.AnonymousUsersAllowed).Description("Allow anonymous users to visit the store ");
-        Field(x => x.IsSpa).Description("SPA");
         Field(x => x.EmailVerificationEnabled).Description("Email address verification enabled");
         Field(x => x.EmailVerificationRequired).Description("Email address verification required");
         Field(x => x.CreateAnonymousOrderEnabled).Description("Allow anonymous users to create orders (XAPI)");
