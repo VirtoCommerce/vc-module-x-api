5a9bd0ee1b8f8f1a41f43ce2f96878f563091a80
Oleg Zhuk <zhukoo@gmail.com>
2024-10-31 15:21:04 +0200

    fix obsolete comment

--- src/VirtoCommerce.Xapi.Core/Extensions/ClaimsPrincipalExtensions.cs
+++ src/VirtoCommerce.Xapi.Core/Extensions/ClaimsPrincipalExtensions.cs
@@ -7,7 +7,7 @@ namespace VirtoCommerce.Xapi.Core.Extensions;
 
 public static class ClaimsPrincipalExtensions
 {
-    [Obsolete("Use VirtoCommerce.Platform.Core.Security.GetUserId extension", DiagnosticId = "VC0009", UrlFormat = "https://docs.virtocommerce.org/platform/user-guide/versions/virto3-products-versions/")]
+    [Obsolete("Use VirtoCommerce.Platform.Core.Security.ClaimsPrincipalExtensions.GetUserId extension", DiagnosticId = "VC0009", UrlFormat = "https://docs.virtocommerce.org/platform/user-guide/versions/virto3-products-versions/")]
     public static string GetCurrentUserId(this ClaimsPrincipal claimsPrincipal)
     {
         return claimsPrincipal?.GetUserId() ?? AnonymousUser.UserName;
