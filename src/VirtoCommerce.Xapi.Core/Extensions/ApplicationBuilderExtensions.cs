961c3578c5af425b9476a3b24a1cb3ec91b0ad5b
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-12-05 15:36:56 +0200

    feat: add UseScopedSchema extension

--- src/VirtoCommerce.Xapi.Core/Extensions/ApplicationBuilderExtensions.cs
+++ src/VirtoCommerce.Xapi.Core/Extensions/ApplicationBuilderExtensions.cs
@@ -1,11 +1,22 @@
 using GraphQL.Server.Ui.Playground;
 using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using VirtoCommerce.Xapi.Core.Infrastructure;
+using VirtoCommerce.Xapi.Core.Models;
 
 namespace VirtoCommerce.Xapi.Core.Extensions;
 
 public static class ApplicationBuilderExtensions
 {
+    public static IApplicationBuilder UseScopedSchema<TMarker>(this IApplicationBuilder builder, string schemaPath)
+    {
+        var playgroundOptions = builder.ApplicationServices.GetService<IOptions<GraphQLPlaygroundOptions>>();
+
+        return builder.UseSchemaGraphQL<ScopedSchemaFactory<TMarker>>(playgroundOptions?.Value?.Enable ?? true, schemaPath);
+    }
+
     public static IApplicationBuilder UseSchemaGraphQL<TSchema>(this IApplicationBuilder builder, bool schemaIntrospectionEnabled = true, string schemaPath = null)
         where TSchema : ISchema
     {
