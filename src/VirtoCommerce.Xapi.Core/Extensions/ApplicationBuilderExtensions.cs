eb9844e340a614013b9baede5b8b558b45a333e6
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-12-09 11:12:24 +0200

    VCST-2241: add UseScopedSchema extension (#19)

--- src/VirtoCommerce.Xapi.Core/Extensions/ApplicationBuilderExtensions.cs
+++ src/VirtoCommerce.Xapi.Core/Extensions/ApplicationBuilderExtensions.cs
@@ -1,11 +1,22 @@
 using GraphQL.Server.Ui.Playground;
 using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using VirtoCommerce.Xapi.Core.Infrastructure;
+using VirtoCommerce.Xapi.Core.Models;
 
 namespace VirtoCommerce.Xapi.Core.Extensions;
 
 public static class ApplicationBuilderExtensions
 {
+    public static IApplicationBuilder UseScopedSchema<TMarker>(this IApplicationBuilder builder, string schemaPath)
+    {
+        var playgroundOptions = builder.ApplicationServices.GetService<IOptions<GraphQLPlaygroundOptions>>();
+
+        return builder.UseSchemaGraphQL<ScopedSchemaFactory<TMarker>>(playgroundOptions?.Value?.Enable ?? true, schemaPath);
+    }
+
     public static IApplicationBuilder UseSchemaGraphQL<TSchema>(this IApplicationBuilder builder, bool schemaIntrospectionEnabled = true, string schemaPath = null)
         where TSchema : ISchema
     {
@@ -15,15 +26,15 @@ public static class ApplicationBuilderExtensions
             graphQlPath = $"{graphQlPath}/{schemaPath}";
         }
 
-        var playgroundPath = "/ui/playground";
-        if (!string.IsNullOrEmpty(schemaPath))
-        {
-            playgroundPath = $"{playgroundPath}/{schemaPath}";
-        }
-
         builder.UseGraphQL<TSchema>(path: graphQlPath);
         if (schemaIntrospectionEnabled)
         {
+            var playgroundPath = "/ui/playground";
+            if (!string.IsNullOrEmpty(schemaPath))
+            {
+                playgroundPath = $"{playgroundPath}/{schemaPath}";
+            }
+
             builder.UseGraphQLPlayground(new PlaygroundOptions
             {
                 GraphQLEndPoint = graphQlPath,
