9cd249cb3b81b7865be9e924e94bfc4f28068523
Artem Dudarev <artem@virtoworks.com>
2025-02-04 17:15:16 +0200

    Make range bounds nullable

--- src/VirtoCommerce.Xapi.Core/Models/Facets/FacetRange.cs
+++ src/VirtoCommerce.Xapi.Core/Models/Facets/FacetRange.cs
@@ -2,18 +2,18 @@ namespace VirtoCommerce.Xapi.Core.Models.Facets
 {
     public sealed class FacetRange
     {
-        public decimal From { get; set; }
+        public decimal? From { get; set; }
         public bool IncludeFrom { get; set; }
         public string FromStr { get; set; }
 
-        public decimal To { get; set; }
+        public decimal? To { get; set; }
         public bool IncludeTo { get; set; }
         public string ToStr { get; set; }
 
         public long Count { get; set; }
         public long Total { get; set; }
-        public long Min { get; set; }
-        public long Max { get; set; }
+        public decimal Min { get; set; }
+        public decimal Max { get; set; }
 
         public bool IsSelected { get; set; }
         public string Label { get; set; }
--- src/VirtoCommerce.Xapi.Core/Schemas/Facets/FacetRangeType.cs
+++ src/VirtoCommerce.Xapi.Core/Schemas/Facets/FacetRangeType.cs
@@ -7,12 +7,12 @@ namespace VirtoCommerce.Xapi.Core.Schemas.Facets
         public FacetRangeType()
         {
             Field(d => d.Count, nullable: false).Description("Amount of products for which the values in a field fall into the specified range");
-            Field(d => d.From, nullable: false).Description("The range’s lower endpoint in number format, 0 represents infinity");
+            Field(d => d.From, nullable: true).Description("The range’s lower endpoint in number format");
             Field(d => d.IncludeFrom, nullable: false).Description("The flag indicates that From exclusive");
             Field(d => d.FromStr, nullable: true).Description("The range’s lower endpoint in string format, empty string represents infinity");
             Field(d => d.Max, nullable: false).Description("Maximum value among all values contained within the range");
             Field(d => d.Min, nullable: false).Description("Minimum value among all values contained within the range");
-            Field(d => d.To, nullable: false).Description("The range’s upper endpoint in number format, 0 represents infinity");
+            Field(d => d.To, nullable: true).Description("The range’s upper endpoint in number format");
             Field(d => d.IncludeTo, nullable: false).Description("The flag indicates that To exclusive");
             Field(d => d.ToStr, nullable: true).Description("The range’s upper endpoint in string format, empty string represents infinity");
             Field(d => d.Total, nullable: false).Description("Sum of all values contained in the range");
