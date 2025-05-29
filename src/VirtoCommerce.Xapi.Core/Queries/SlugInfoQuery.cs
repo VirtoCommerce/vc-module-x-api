de0a5feaee617943060763b76a5fae64c20ad7dc
Basil Kotov <basilkot@gmail.com>
2025-05-29 07:24:17 +0100

    revert sluginfoquery changes

--- src/VirtoCommerce.Xapi.Core/Queries/SlugInfoQuery.cs
+++ src/VirtoCommerce.Xapi.Core/Queries/SlugInfoQuery.cs
@@ -5,28 +5,29 @@ using VirtoCommerce.Xapi.Core.BaseQueries;
 using VirtoCommerce.Xapi.Core.Extensions;
 using VirtoCommerce.Xapi.Core.Models;
 
-namespace VirtoCommerce.Xapi.Core.Queries;
-
-public class SlugInfoQuery : Query<SlugInfoResponse>
+namespace VirtoCommerce.Xapi.Core.Queries
 {
-    public string Permalink { get; set; }
-    public string StoreId { get; set; }
-    public string UserId { get; set; }
-    public string CultureName { get; set; }
-
-    public override IEnumerable<QueryArgument> GetArguments()
+    public class SlugInfoQuery : Query<SlugInfoResponse>
     {
-        yield return Argument<StringGraphType>(nameof(Permalink));
-        yield return Argument<StringGraphType>(nameof(StoreId));
-        yield return Argument<StringGraphType>(nameof(UserId));
-        yield return Argument<StringGraphType>(nameof(CultureName));
-    }
+        public string Permalink { get; set; }
+        public string StoreId { get; set; }
+        public string UserId { get; set; }
+        public string CultureName { get; set; }
 
-    public override void Map(IResolveFieldContext context)
-    {
-        Permalink = context.GetArgument<string>(nameof(Permalink));
-        StoreId = context.GetArgument<string>(nameof(StoreId));
-        UserId = context.GetCurrentUserId();
-        CultureName = context.GetArgument<string>(nameof(CultureName));
+        public override IEnumerable<QueryArgument> GetArguments()
+        {
+            yield return Argument<StringGraphType>(nameof(Permalink));
+            yield return Argument<StringGraphType>(nameof(StoreId));
+            yield return Argument<StringGraphType>(nameof(UserId));
+            yield return Argument<StringGraphType>(nameof(CultureName));
+        }
+
+        public override void Map(IResolveFieldContext context)
+        {
+            Permalink = context.GetArgument<string>(nameof(Permalink));
+            StoreId = context.GetArgument<string>(nameof(StoreId));
+            UserId = context.GetCurrentUserId();
+            CultureName = context.GetArgument<string>(nameof(CultureName));
+        }
     }
 }
