2e44ce17492200308bd93a53c1637506a363b333
Basil Kotov <basilkot@gmail.com>
2025-05-29 07:28:50 +0100

    revert SlugInfoQueryBuilder changes

--- src/VirtoCommerce.Xapi.Data/Queries/SlugInfoQueryBuilder.cs
+++ src/VirtoCommerce.Xapi.Data/Queries/SlugInfoQueryBuilder.cs
@@ -5,10 +5,15 @@ using VirtoCommerce.Xapi.Core.Models;
 using VirtoCommerce.Xapi.Core.Queries;
 using VirtoCommerce.Xapi.Core.Schemas;
 
-namespace VirtoCommerce.Xapi.Data.Queries;
-
-public class SlugInfoQueryBuilder(IMediator mediator, IAuthorizationService authorizationService)
-    : QueryBuilder<SlugInfoQuery, SlugInfoResponse, SlugInfoResponseType>(mediator, authorizationService)
+namespace VirtoCommerce.Xapi.Data.Queries
 {
-    protected override string Name => "slugInfo";
+    public class SlugInfoQueryBuilder : QueryBuilder<SlugInfoQuery, SlugInfoResponse, SlugInfoResponseType>
+    {
+        public SlugInfoQueryBuilder(IMediator mediator, IAuthorizationService authorizationService)
+            : base(mediator, authorizationService)
+        {
+        }
+
+        protected override string Name => "slugInfo";
+    }
 }
