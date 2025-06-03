4bbb3d015e93f9c9aef65c6327155a9f52abc9fe
Basil Kotov <basilkot@gmail.com>
2025-06-03 10:20:14 +0100

    return Slug argument into SlugInfo query

--- src/VirtoCommerce.Xapi.Core/Queries/SlugInfoQuery.cs
+++ src/VirtoCommerce.Xapi.Core/Queries/SlugInfoQuery.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using GraphQL;
 using GraphQL.Types;
@@ -9,13 +10,23 @@ namespace VirtoCommerce.Xapi.Core.Queries
 {
     public class SlugInfoQuery : Query<SlugInfoResponse>
     {
-        public string Permalink { get; set; }
+        private string _permalink;
+
+        [Obsolete("Use Permalink property", DiagnosticId = "VC0008", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
+        public string Slug { get; set; }
+
+#pragma warning disable VC0008 // Type or member is obsolete
+        public string Permalink { get => _permalink ?? Slug; set => _permalink = value; }
+#pragma warning restore VC0008
         public string StoreId { get; set; }
         public string UserId { get; set; }
         public string CultureName { get; set; }
 
         public override IEnumerable<QueryArgument> GetArguments()
         {
+#pragma warning disable VC0008 // Type or member is obsolete
+            yield return Argument<StringGraphType>(nameof(Slug));
+#pragma warning restore VC0008
             yield return Argument<StringGraphType>(nameof(Permalink));
             yield return Argument<StringGraphType>(nameof(StoreId));
             yield return Argument<StringGraphType>(nameof(UserId));
@@ -24,6 +35,9 @@ namespace VirtoCommerce.Xapi.Core.Queries
 
         public override void Map(IResolveFieldContext context)
         {
+#pragma warning disable VC0008 // Type or member is obsolete
+            Slug = context.GetArgument<string>(nameof(Slug));
+#pragma warning restore VC0008
             Permalink = context.GetArgument<string>(nameof(Permalink));
             StoreId = context.GetArgument<string>(nameof(StoreId));
             UserId = context.GetCurrentUserId();
