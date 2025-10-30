6d22f215046ccfe8c639c4911de261dac64f76fc
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-10-30 17:34:54 +0200

    fix: sonar

--- src/VirtoCommerce.Xapi.Core/Models/OptionalGraphQlTypesContainerAttribute.cs
+++ src/VirtoCommerce.Xapi.Core/Models/OptionalGraphQlTypesContainerAttribute.cs
@@ -5,10 +5,12 @@ namespace VirtoCommerce.Xapi.Core.Models;
 [AttributeUsage(AttributeTargets.Class)]
 public class OptionalGraphQlTypesContainerAttribute : Attribute
 {
-    public string DependencyName { get; set; }
+    public string DependencyName { get; }
 
     public OptionalGraphQlTypesContainerAttribute(string dependencyName)
     {
+        ArgumentNullException.ThrowIfNull(dependencyName);
+
         DependencyName = dependencyName;
     }
 }
