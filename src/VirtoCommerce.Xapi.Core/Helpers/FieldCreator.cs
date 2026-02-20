27d7c2947c3118e5a9e5107b4adc115460598288
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2026-02-20 09:34:28 +0200

    VCST-4467: Add GraphType hook and custom schema (#60)

---
+++ src/VirtoCommerce.Xapi.Core/Helpers/FieldCreator.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Resolvers;
+using GraphQL.Types;
+using VirtoCommerce.Xapi.Core.Extensions;
+
+namespace VirtoCommerce.Xapi.Core.Helpers
+{
+    public static class FieldCreator
+    {
+        public static FieldType CreateField<TSourceType, TGraphType>(
+          string name,
+          string description = null,
+          QueryArguments arguments = null,
+          Func<IResolveFieldContext<TSourceType>, object> resolve = null,
+          string deprecationReason = null)
+            where TGraphType : IGraphType
+        {
+            return new FieldType
+            {
+                Name = name,
+                Description = description,
+                DeprecationReason = deprecationReason,
+                Type = GraphTypeExtensionHelper.GetActualComplexType<TGraphType>(),
+                Arguments = arguments,
+                Resolver = resolve != null
+                    ? new FuncFieldResolver<TSourceType, object>(context =>
+                    {
+                        context.CopyArgumentsToUserContext();
+                        return resolve(context);
+                    })
+                    : null
+            };
+        }
+
+        public static FieldType CreateFieldAsync<TSourceType, TGraphType>(
+          string name,
+          string description = null,
+          QueryArguments arguments = null,
+          Func<IResolveFieldContext<TSourceType>, Task<object>> resolve = null,
+          string deprecationReason = null)
+            where TGraphType : IGraphType
+        {
+            return new FieldType
+            {
+                Name = name,
+                Description = description,
+                DeprecationReason = deprecationReason,
+                Type = GraphTypeExtensionHelper.GetActualComplexType<TGraphType>(),
+                Arguments = arguments,
+                Resolver = resolve != null
+                    ? new FuncFieldResolver<TSourceType, object>(context =>
+                    {
+                        context.CopyArgumentsToUserContext();
+                        return new ValueTask<object>(resolve(context));
+                    })
+                    : null
+            };
+        }
+    }
+}
