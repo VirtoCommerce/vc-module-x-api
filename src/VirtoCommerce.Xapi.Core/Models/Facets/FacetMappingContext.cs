715adfd0ae5ec9ed22680f9f38794409366cdad0
Dmitry Grishin <Dmitry.Grishin@virtoworks.com>
2026-08-18 18:40:10 +0400

    VCST-5661: Implemented FacetMappingContext

---
+++ src/VirtoCommerce.Xapi.Core/Models/Facets/FacetMappingContext.cs
@@ -0,0 +1,8 @@
+namespace VirtoCommerce.Xapi.Core.Models.Facets
+{
+    public class FacetMappingContext
+    {
+        public string CultureName { get; set; }
+        public int? Order { get; set; }
+    }
+}
